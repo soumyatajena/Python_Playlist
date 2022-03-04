e013fee99b64941d123f0854226ba9a1149fe9ea
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-05 00:25:13 +0530

    Added set methods

--- Set.py
+++ Set.py
@@ -26,3 +26,41 @@ print(list(my_set)) # [1, 2, 3, 4, 5, 6, -1]
 my_set.clear()
 print(new_set) # {1, 2, 3, 4, 5, 6, -1}
 print(my_set) # empty set()
+
+## *************************** Set Methods ******************************************
+
+# 1. .difference() - operates as A-B in sets
+my_set={1,2,3,4,5}
+your_set={3,4,9,0,1}
+print(my_set.difference(your_set)) # {2, 3, 5}
+print(your_set.difference(my_set)) # {0, 9}
+
+# 2. .difference_update - modifies the given set after operation
+print(my_set) # {1, 2, 3, 4, 5}
+my_set.difference_update(your_set)
+print(my_set) # {2, 3, 5}
+
+# 3. .discard() - remove the value and update the set
+my_set.discard(5)
+print(my_set) # {2, 3}
+
+# 4. .intersection()- A int B
+print(my_set.intersection(your_set))
+print(my_set & your_set) 
+
+#5. .isdisjoint()- are the two sets have anything in common?
+# True - nothing in common
+print(my_set.isdisjoint(your_set)) # True
+
+#6. .union - added both sets unique elements
+print(my_set.union(your_set)) # {0, 1, 2, 3, 4, 9}
+print(my_set | your_set) # {0, 1, 2, 3, 4, 9}
+
+#7. .issubset()
+my_set={3,4}
+your_set={3,4,9,0,1}
+print(my_set.issubset(your_set)) # True
+
+#8. .issuperset
+print(my_set.issuperset(your_set)) # False
+print(your_set.issuperset(my_set)) # True

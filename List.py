dd6b714b5ab36b248940585ce90d2fb46cc66d4d
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-04 21:34:19 +0530

    List methods added to List.py

--- List.py
+++ List.py
@@ -29,3 +29,62 @@ print(f'Modified New list:{new_list}')
 ## List Slicing
 print(f'New list sliced:{amazon_list[0:4:2]}') #'laptop', 'pencil'
 
+
+## *************************** List methods ******************************************
+
+integer_list = [45,0,9,-1,5,8,8]
+
+# 1. Sort() - sortlist elements in ascending order
+integer_list.sort() # does not return anything - None 
+print(f'Sort() list is: {integer_list}') 
+integer_list.sort(reverse=True)
+print(f'Sort(Desc) list is: {integer_list}') 
+
+# NOTE : sorted() method accepts any iterable whereas the sort() method only works with lists.
+
+# 2. Sorted() - sort the elements and returns the sorted list
+sorted(integer_list)
+print(f'Sorted(Asc) list is: {integer_list}') 
+sorted(integer_list,reverse=False)
+print(f'Sorted(Desc) list is: {integer_list}') 
+
+# 3. Count - count the number of similar elements in the list
+print(f'No of occurance of 8 is: {integer_list.count(8)}')
+
+# 4. Append - add value to list
+integer_list.append(10)    # does not return anything - None
+print(f"Appended list: {integer_list}")
+
+# 5. Pop - removes the last value from list
+popped_value= integer_list.pop()
+print(f"Default popped list: {integer_list}")
+popped_value = integer_list.pop(2)
+print(f"Index 2 popped value is : {popped_value} and popped list: {integer_list}")
+
+# 6. Remove - remove the given value
+integer_list.remove(-1) # does not return anything - None
+print(f"list after removal of 1 is: {integer_list}")
+
+# 7. Reverse - reverses the list
+integer_list.reverse()
+print(f'Reversed list:{integer_list}')
+integer_list.reverse()
+print(f'Double Reversed[Asc] list:{integer_list[::]}')
+
+# 8. Range - print from starting given value to ending value
+print(list(range(-2,101)))
+
+# 9. Clear - clears the entire list and makes the list empty []
+integer_list.clear() # does not return anything - None
+print(f"Cleared list: {integer_list}")
+
+# 10. Join - string method- joins the string on basis of the given original string
+
+str_list="!"
+new_str=str_list.join(['hi','my', 'name','is','Soumyata'])
+str_list=" "
+new_str=str_list.join(['hi','my', 'name','is','Soumyata'])
+# or
+new_str=' '.join(['hi','my', 'name','is','Soumyata'])
+print(new_str)
+

fd4f0a9cd0b0bbaf613157c6b58612a6ec1c9c5f
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-09 18:40:37 +0530

    Added Conditionals including Ternary and ShortCircuit

--- Conditional.py
+++ Conditional.py
@@ -1,5 +1,4 @@
-from doctest import FAIL_FAST
-
+# Conditional Syntax
 
 age=input('Enter your age:')
 if int(age)>18:
@@ -18,3 +17,51 @@ elif is_licenced:
     print('Hurray you have a learner license !')
 else:
     print("Wait a bit longer to drive :(")
+
+## ------------------------- Truthy v/s Falsy --------------------
+
+username='Soumyata'
+password = 12345
+
+# here username and password is evaluated as boolean value 
+if username and password: 
+    print('User Exists')
+else:
+    print('Please register!!')
+
+# o/p - User Exists
+
+## ------------------------- Ternary Operator --------------------
+
+is_friend=True
+
+# condition_if_true if condition else condition_if_else
+can_message="message allowed" if is_friend else 'not allowed to message'
+print(can_message) # message allowed
+
+
+## ------------------------- Logical Operators --------------------
+
+print( 0 != 1) # True - coz 0 is not equal to 1
+print( 0 != 0) # False - coz 0 is equal to 0
+
+# not - operator is both function and keyword
+print(not(True)) # False
+print(not(1==1)) # False
+
+## ------------------------- is v/s == --------------------
+
+#Python does not convert type when checking with comparison operature
+
+# == - checks the value
+print(True == 1) # True
+
+# is - checks the memory location
+print(True is 1) # False
+
+# WARNING - "is" with a literal. Did you mean "=="?
+print('1' is '1') # True
+
+# EXCEPTION - empty lists takes separate memory location
+print([] is []) # False
+

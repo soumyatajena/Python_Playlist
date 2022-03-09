962d11f63e9cf39732c268257aa0c099c8fb1030
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-09 20:12:24 +0530

    Added Enumerate func

---
+++ Enumerate.py
@@ -0,0 +1,22 @@
+# Enumerate provides you the elements in a iterable collection with its "Index" value
+
+
+for i,char in enumerate('Python is crazy!'):
+    print(i, char)
+
+# 0 P
+# 1 y
+# 2 t
+# 3 h
+# 4 o
+# 5 n
+# 6
+# 7 i
+# 8 s
+# 9
+# 10 c
+# 11 r
+# 12 a
+# 13 z
+# 14 y
+# 15 !

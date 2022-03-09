550aa2c689b3b82766d78f2275823942b8a0a975
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-09 18:57:21 +0530

    Added Loop str

---
+++ Loop.py
@@ -0,0 +1,27 @@
+for item in (1,2,3,4,'A'):
+    for x in ['a','b','c']:
+        print(x)
+    print(item)
+print(item)
+
+# a
+# b
+# c
+# 1
+# a
+# b
+# c
+# 2
+# a
+# b
+# c
+# 3
+# a
+# b
+# c
+# 4
+# a
+# b
+# c
+# A
+# A

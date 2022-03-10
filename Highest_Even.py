5ca73a27ed1512658845c4c7ee91b9504a1c7361
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-10 19:18:33 +0530

    Added program to find the highest even number

---
+++ Highest_Even.py
@@ -0,0 +1,24 @@
+def highest_even(num_list):
+    highest=0
+    for item in num_list:
+        if item%2==0 and item>highest:
+            highest=item
+    print(f'Highest even among {num_list} is {highest}')
+
+num_list=[11,10,56,9,1,-3]
+highest_even(num_list)
+
+# Highest even among [11, 10, 56, 9, 1, -3] is 56
+
+# OR
+
+def highest_even(num_list):
+    evens=[]
+    for item in num_list:
+        if item%2==0:
+            evens.append(item)
+    return max(evens)
+
+print(highest_even(num_list=[11,10,56,9,1,-3]))
+
+# 56

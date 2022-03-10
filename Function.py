e8226247cf531fe9ccc3df132ba6f3615bac05e1
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-10 16:26:38 +0530

    Installed autopep 8 formatter

--- Function.py
+++ Function.py
@@ -3,24 +3,31 @@
 def say_hello():
     print('Hello!')
 
+
 say_hello()
 # Hello!
 
 #-----------------------------------------------------------
 
 # parameters
+
+
 def say_hello(name, emoji):
     print(f'Hello {name}{emoji}')
 
+
 # positional arguments - position matters for args
-say_hello('Soumyata',' 😊')
+say_hello('Soumyata', ' 😊')
 # Hello Soumyata 😊
 
 #------------------------------------------------------------
 
 # Default Parameters
+
+
 def say_hello(emoji=' 😎', name='Default Name'):
     print(f'Hello {name}{emoji}')
+
 
 # keyword arguments - not advised
 say_hello(emoji=' 😊', name='Soumyata')
@@ -31,7 +38,9 @@ say_hello()
 
 #-------------------- FUNC WITH RETURN TYPE ---------------
 
-def sum(num1,num2):
+
+def sum(num1, num2):
     return num1+num2
 
-print(sum(8,9)) # 17
+
+print(sum(8, 9))  # 17

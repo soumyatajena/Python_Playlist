31771b7073fe9d9364fe684a3f09771f67f006c7
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-10 16:16:55 +0530

    Added Functions basics

---
+++ Function.py
@@ -0,0 +1,37 @@
+# def - define function
+
+def say_hello():
+    print('Hello!')
+
+say_hello()
+# Hello!
+
+#-----------------------------------------------------------
+
+# parameters
+def say_hello(name, emoji):
+    print(f'Hello {name}{emoji}')
+
+# positional arguments - position matters for args
+say_hello('Soumyata',' 😊')
+# Hello Soumyata 😊
+
+#------------------------------------------------------------
+
+# Default Parameters
+def say_hello(emoji=' 😎', name='Default Name'):
+    print(f'Hello {name}{emoji}')
+
+# keyword arguments - not advised
+say_hello(emoji=' 😊', name='Soumyata')
+# Hello Soumyata 😊
+
+say_hello()
+# Hello Default Name 😎
+
+#-------------------- FUNC WITH RETURN TYPE ---------------
+
+def sum(num1,num2):
+    return num1+num2
+
+print(sum(8,9)) # 17

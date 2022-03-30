84f2c3fd892c73bd6e57bd010bd80764ac44ef00
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-03-30 21:49:13 +0530

    Added templates and static folder

--- Web Server/server.py
+++ Web Server/server.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__) # __main__   -> name of the app
 @app.route("/") # decorator
-def hello_world():
+def hello():
     return "<p>Hello, Soumyata!</p>"
+
+@app.route("/renderHtml") 
+def renderHtml():
+    return render_template('index.html')
+
+# @app.route("/favicon.ico") 
+# def favicon():
+#     return render_template('index.html')
